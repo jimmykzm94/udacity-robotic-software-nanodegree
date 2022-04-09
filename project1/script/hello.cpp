8695fb557c14ce55dce6fe54aaf4a6b2f7141569
Jimmy from udacity vm <jimmykzm94@gmail.com>
2022-04-09 13:13:38 +0000

    Add project 1

---
+++ project1/script/hello.cpp
@@ -0,0 +1,13 @@
+#include <gazebo/gazebo.hh>
+
+namespace gazebo{
+    class WorldPluginRobot : public WorldPlugin{
+        public : WorldPluginRobot():WorldPlugin(){
+            printf("Welcome to JK's World!\n");
+        }
+        public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf){
+
+        }
+    };
+    GZ_REGISTER_WORLD_PLUGIN(WorldPluginRobot)
+}
