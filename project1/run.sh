52fcb99f04841473d96b1a8111747ed43475615b
kzming <jimmykzm94@gmail.com>
2022-04-14 08:00:33 +0000

    Update shell

--- project1/run.sh
+++ project1/run.sh
@@ -2,4 +2,4 @@ mkdir -p build && cd build
 cmake ../ && make && cd ..
 # Uncomment to set plugin (to print Welcome message)
 # export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:/home/workspace/udacity-robotic-software-nanodegree/project1/build
-gazebo world/myworld
+gazebo world/ksl_world
